27f0a62e58c9425948d5bc61e5209cc0dac3c89f
Benedict.ESN <elder@elderscrolls.net>
2024-09-15 23:02:25 +0300

    Added Spring feature/di-annotation

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,7 +1,8 @@
 package ru.netology.repository;
 
-import ru.netology.model.Post;
+import org.springframework.stereotype.Repository;
 import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,38 +10,39 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+@Repository
 public class PostRepository {
-  private final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
-  private final AtomicLong idCounter = new AtomicLong();
+    private final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicLong idCounter = new AtomicLong();
 
-  public List<Post> all() {
-    return new ArrayList<>(posts.values());
-  }
-
-  public Optional<Post> getById(long id) {
-    return Optional.ofNullable(posts.get(id));
-  }
-
-  public Post save(Post post) {
-    if (post.getId() == 0) {
-      // Создаём новый пост
-      long newId = idCounter.incrementAndGet();
-      post.setId(newId);
-      posts.put(newId, post);
-    } else {
-      // Обновляем пост
-      if (posts.containsKey(post.getId())) {
-        posts.put(post.getId(), post);
-      } else {
-        throw new NotFoundException("Post with id " + post.getId() + " not found");
-      }
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
     }
-    return post;
-  }
 
-  public void removeById(long id) {
-    if (posts.remove(id) == null) {
-      throw new NotFoundException("Post with id " + id + " not found");
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id));
     }
-  }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            // Создаём новый пост
+            long newId = idCounter.incrementAndGet();
+            post.setId(newId);
+            posts.put(newId, post);
+        } else {
+            // Обновляем пост
+            if (posts.containsKey(post.getId())) {
+                posts.put(post.getId(), post);
+            } else {
+                throw new NotFoundException("Post with id " + post.getId() + " not found");
+            }
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        if (posts.remove(id) == null) {
+            throw new NotFoundException("Post with id " + id + " not found");
+        }
+    }
 }
