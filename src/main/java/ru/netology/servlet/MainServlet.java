ab6bb163b67c7855d35fdbdac2a41c6530459e15
Benedict.ESN <elder@elderscrolls.net>
2024-09-15 23:07:43 +0300

    Added Spring feature/di-java

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,6 +1,7 @@
 package ru.netology.servlet;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.AppConfig;
 import ru.netology.controller.PostController;
 
 import javax.servlet.http.HttpServlet;
@@ -15,7 +16,7 @@ public class MainServlet extends HttpServlet {
 
   @Override
   public void init() {
-    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("ru.netology");
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
     controller = context.getBean(PostController.class);
   }
 
